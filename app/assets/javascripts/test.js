f2d18b0de5f482d80431d2be7d5e4a192fdd9f1c
batu <304701204@qq.com>
2017-10-16 09:17:00 +0800

    create

---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,11 @@
+// function selectFileImage(fileObj) {  
+// 	var file = fileObj.files['0']; 
+
+// 	EXIF.getData(file, function() {  
+// 	    //alert(EXIF.pretty(file));
+// 	    //var keypar = EXIF.getAllTags(this));   
+// 	    alert(EXIF.getTag(this, 'GPSLatitude'));   
+	     
+// 	    //return;  
+// 	});  
+// }  
